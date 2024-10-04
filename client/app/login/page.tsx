6e6cd46077f6e140e52c040603abb270358ded27
Christopher4113 <ch.lam1328@gmail.com>
2024-10-04 01:41:32 -0400

    added login and signup pages

---
+++ client/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginPages = () => {
+  return (
+    <div className='flex'>
+        <h1 className='text-center text-black text-2xl'>Login</h1>
+    </div>
+  )
+}
+
+export default LoginPages
---
+++ client/app/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signuppage = () => {
+  return (
+    <div className='flex'>
+        <h1 className='text-center text-black text-2xl'>Signup</h1>
+    </div>
+  )
+}
+
+export default Signuppage
