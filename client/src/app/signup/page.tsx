3a957b1b2c0a17b7b34b9c76fc52e4d51eaf1dd5
Christopher4113 <ch.lam1328@gmail.com>
2024-10-07 00:01:28 -0400

    made the signup page

--- client/src/app/signup/page.tsx
+++ client/src/app/signup/page.tsx
@@ -1,9 +1,46 @@
+"use client";
 import React from 'react'
+import Link from "next/link";
+import { useRouter } from 'next/navigation';
+import "./styles.css";
+//import {axios} from 'axios';
 
 const Signuppage = () => {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: ""
+  });
+  const onSignup = async () => {    
+  }
+
   return (
-    <div className='flex'>
-        <h1 className='text-center text-black text-2xl'>Signup</h1>
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+        <div className="form-box">
+          <form className="form">
+            <span className="title">Sign up</span>
+            <span className="subtitle">
+              Create a free account with your email.
+            </span>
+            <div className="form-container">
+              <input type="text" className="input" placeholder="Username" id='username'value = {user.username} 
+              onChange={(e) => setUser({...user,username: e.target.value})} />
+              <input type="email" className="input" placeholder="Email" id='email' value = {user.email} 
+              onChange={(e) => setUser({...user,email: e.target.value})} />
+              <input type="password" className="input" placeholder="Password" id='password' value = {user.password} 
+              onChange={(e) => setUser({...user,password: e.target.value})} />
+            </div>
+            <button>Sign up</button>
+          </form>
+          <div className="form-section">
+            <p>
+              Have an account? <a href="">Log in</a>{" "}
+            </p>
+          </div>
+        </div>
+
+
+
     </div>
   )
 }
