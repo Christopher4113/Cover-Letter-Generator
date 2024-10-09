ef45a3d9256a1192b8c54e8176777db277ab7c81
Christopher4113 <ch.lam1328@gmail.com>
2024-10-09 00:06:58 -0400

    new changes

--- client/app/signup/page.tsx
+++ client/app/signup/page.tsx
@@ -1,24 +1,48 @@
 "use client";
-import React from 'react'
+import React, { useEffect } from 'react'
 import Link from "next/link";
 import { useRouter } from 'next/navigation';
 import "./styles.css";
-//import {axios} from 'axios';
+import { toast } from 'react-hot-toast';
+import axios from 'axios';
 
 const SignupPage = () => {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
     username: ""
   });
-  const onSignup = async () => {    
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  const onSignup = async () => {
+    try {
+      setLoading(false)
+      const response = await axios.post("/api/users/signup",user);
+      console.log("Signup success", response.data);
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Signup failed", error.message);
+      toast.error(error.message);
+    }finally {
+      setLoading(false);
+    }    
   }
+
+  useEffect(() => {
+    if(user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  },[user])
 
   return (
     <div className='flex flex-col items-center justify-center min-h-screen py-2'>
         <div className="form-box">
           <form className="form">
-            <span className="title">Sign up</span>
+            <span className="title">{loading ? "Processing" : "Signup"}</span>
             <span className="subtitle">
               Create a free account with your email.
             </span>
@@ -30,7 +54,7 @@ const SignupPage = () => {
               <input type="password" className="input" placeholder="Password" id='password' value = {user.password} 
               onChange={(e) => setUser({...user,password: e.target.value})} />
             </div>
-            <button onClick={onSignup}>Sign up</button>
+            <button onClick={onSignup}>{buttonDisabled ? "No Signup" : "Signup"}</button>
           </form>
           <div className="form-section">
             <p>
