5bf9afa90b142f4dc9edfbfe200312f214dc871a
Christopher4113 <ch.lam1328@gmail.com>
2024-10-13 18:07:52 -0400

    mailer part 2

--- client/helpers/mailer.ts
+++ client/helpers/mailer.ts
@@ -7,9 +7,27 @@ export const sendEmail = async({email,emailType, userId}: any) => {
         //create a hash token
         const hashedToken = await bcryptjs.hash(userId.toString(),10)
 
-        await User.findByIdAndUpdate(userId, {
-            verifyToken: hashedToken,
-            verifyTokenExpiry: Date.now() + 3600000})
+        if (emailType === "VERIFY") {
+            await User.findByIdAndUpdate(userId, {
+                verifyToken: hashedToken,
+                verifyTokenExpiry: Date.now() + 3600000})
+        } else if (emailType === "RESET") {
+            await User.findByIdAndUpdate(userId, {
+                forgotPasswordToken: hashedToken,
+                forgotPasswordTokenExpiry: Date.now() + 3600000})
+        }
+
+        // Looking to send emails in production? Check out our Email API/SMTP product!
+        var transport = nodemailer.createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: "0302e75535a355",
+                pass: "071938eb21f6b3"
+            }
+        });
+
+
     } catch (error: any) {
         throw new Error(error.message);
     }
