7b4c2637c7aa4c18ba75a160724603706b4d9dcb
Christopher4113 <ch.lam1328@gmail.com>
2024-10-12 21:49:38 -0400

    finished the profile and id

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 import axios from "axios";
 import Link from "next/link";
+
 import {toast} from "react-hot-toast"
-import React from 'react'
+import React, {useState} from 'react'
 import { useRouter } from "next/navigation";
 import './profile.css';
 const ProfilePage = () => {
   const router = useRouter();
+  const [data,setData] = useState("nothing")
   const logout = async (e: any) => {
     try {
       e.preventDefault();
@@ -19,12 +21,20 @@ const ProfilePage = () => {
       toast.error(error.message);
     }
   }
+  const getUserDetails = async () => {
+    const res = await axios.get('/api/users/me');
+    console.log(res.data);
+    setData(res.data.data._id)
+  }
   return (
     <div className='centered-container'>
        <h1>Profile</h1>
        <hr />
        <p>Profile Page</p>
+       <h2 className="nothing">{data === 'nothing' ? "Nothing" : <Link href={`/profile/${data}`}>{data}</Link>}</h2>
+       <br />
        <button onClick={logout} className="Logout">Logout</button>
+       <button onClick={getUserDetails}>GetUser Details</button>
     </div>
   )
 }
