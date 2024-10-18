3d4b21d74867ba70634ca977944099521d29e2ec
Christopher4113 <ch.lam1328@gmail.com>
2024-10-17 20:01:09 -0400

    finished the start page

--- client/app/start/page.tsx
+++ client/app/start/page.tsx
@@ -4,15 +4,25 @@ import React from 'react'
 import './start.css';
 import Link from "next/link";
 
-const StartPage = () => {
+export default function StartPage() {
   return (
-    <div className='background'>
+    <div className="container">
+      {/* Colorful gradient background */}
+      <div className="background">
+        <div className="svg-pattern" />
+      </div>
+
+      {/* Content */}
+      <div className="content">
+        <h1 className="title">
+          Welcome to the<br />
+          <span className="highlight">AI-Cover-Letter Generator</span>
+        </h1>
         <Link href="/login" className="btn">
-            <span>Get started</span>
+          <span>Get Started</span>
         </Link>
-
+          
+      </div>
     </div>
-  )
+  );
 }
-
-export default StartPage
