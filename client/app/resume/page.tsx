91a959a908829850da6b91b0e973ee0680667552
Christopher4113 <ch.lam1328@gmail.com>
2024-10-19 13:52:34 -0400

    pdf so far

--- client/app/resume/page.tsx
+++ client/app/resume/page.tsx
@@ -1,11 +1,14 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from "next/navigation";
 import axios from 'axios';
 import './resume.css'
 
 const ResumePage = () => {
+  const [title, setTitle] = useState("");
+  const [file, setFile] = useState<File | null>(null); // File type for file
+
   const router = useRouter();
 
   const logout = async (e: React.MouseEvent) => {
@@ -17,14 +20,63 @@ const ResumePage = () => {
       console.log(error.message);
     }
   };
+
+  const submitImage = async (e: any) => {
+    e.preventDefault();
+    try {
+      if (!file) {
+        console.log("No file selected.");
+        return;
+      }
+      const formData = new FormData();
+      formData.append("title", title);
+      formData.append("file", file);
+      console.log(title, file);
+    } catch (error:any) {
+      if (error.response && error.response.data && error.response.data.error) {
+        console.log("Signup failed", error.response.data.error); // Log the specific error
+        alert(error.response.data.error); // Show the error message in an alert
+      } else {
+          console.log("Signup failed", error.message);
+          alert("Signup failed: " + error.message);
+      }
+    }
+    setTitle("")
+    setFile(null)
+  };
+
   return (
     <div className='background'>
+      <div className='background'>
+        <form className='formStyle' onSubmit={submitImage}>
+          <h4>Upload Resume PDF only</h4><br />
+          <div className='inputContainer'>
+            <input
+              type='text'
+              className='form-control'
+              placeholder='Title'
+              required
+              onChange={(e) => setTitle(e.target.value)}
+            /><br />
+            <input
+              type='file'
+              className='form-control'
+              accept='application/pdf'
+              required
+              onChange={(e) => {
+                if (e.target.files && e.target.files.length > 0) {
+                  setFile(e.target.files[0]); // Set the first file if exists
+                }
+              }}
+            />
+          </div>
+          <button className='btn-primary' type='submit'>
+            Submit
+          </button>
+        </form>
+      </div>
 
-
-
-      
-      
-      <Link href="/profile" className='menu-button'> Menu </Link>
+      <Link href="/profile" className='menu-button'>Menu</Link>
       <button onClick={logout} className="Logout">
         Logout
       </button>
@@ -32,4 +84,4 @@ const ResumePage = () => {
   )
 }
 
-export default ResumePage
+export default ResumePage;
