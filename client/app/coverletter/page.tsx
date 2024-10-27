a59a8afff007c5a28b788edd3a0710fb761c2f2c
Christopher4113 <ch.lam1328@gmail.com>
2024-10-26 22:41:34 -0400

    initalized the cover letter portion

--- client/app/coverletter/page.tsx
+++ client/app/coverletter/page.tsx
@@ -1,10 +1,38 @@
 "use client"
-
-import React from 'react'
+import './cover.css';
+import { useEdgeStore } from '@/lib/edgestore';
+import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from "next/navigation";
+import axios from 'axios';
 
 const CoverLetterPage = () => {
+
+  const {edgestore} = useEdgeStore();
+  const router = useRouter();
+  const [info,setInfo] = useState([])
+
+
+  const logout = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    try {
+        await axios.get("/api/users/logout");
+        router.push("/login");
+    } catch (error: any) {
+        console.log(error.message);
+    }
+  };
+  useEffect(() => {
+
+  },[])
+
   return (
-    <div>page</div>
+    <div className='coverBackground'>
+         <Link href="/profile" className='menu-button'>Menu</Link>
+          <button onClick={logout} className="Logout">
+              Logout
+          </button>
+    </div>
   )
 }
 
