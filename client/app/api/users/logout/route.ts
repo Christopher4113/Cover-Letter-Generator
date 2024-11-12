aad1b5687eb03ee4aa96ffdc4e8d94f54925de8a
Christopher4113 <ch.lam1328@gmail.com>
2024-11-12 01:06:56 -0500

    tokens

--- client/app/api/users/logout/route.ts
+++ client/app/api/users/logout/route.ts
@@ -10,11 +10,7 @@ export async function GET() {
         // Set cookie expiration to the past to clear it
         response.cookies.set("token", "", {
             httpOnly: true,  // Ensure the cookie is only accessible via HTTP
-            secure: process.env.NODE_ENV === 'production',  // Set to true only in production for secure cookies
-            expires: new Date(0),  // Expire the cookie
-            path: '/',  // Ensure the cookie is cleared across all paths
-            sameSite: 'strict',  // Use lowercase "strict"
-            domain: 'cover-letter-generator-pi.vercel.app'
+            expires: new Date(0),  // Expire the cookie 
         });
         return response
     } catch (error: any) {
