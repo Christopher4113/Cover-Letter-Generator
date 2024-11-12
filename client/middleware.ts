249969f59929a4e0742485be33c75c7d48ed172d
Christopher4113 <ch.lam1328@gmail.com>
2024-11-12 00:02:38 -0500

    i fixed pathing in middleware

--- client/middleware.ts
+++ client/middleware.ts
@@ -5,11 +5,15 @@ import { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname
     const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail' || path ==='/forgot' || path === '/verifypassword' || path === '/start'
-
+    const isDifferentPath = path === '/';
     const token = request.cookies.get('token')?.value || ''
 
     if(isPublicPath && token) {
-        return NextResponse.redirect(new URL('/login', request.nextUrl))
+        return NextResponse.redirect(new URL('/profile', request.nextUrl))
+    }
+
+    if (isDifferentPath && token) {
+        return NextResponse.redirect(new URL('/profile', request.nextUrl))
     }
 
     if (!isPublicPath && !token) {
