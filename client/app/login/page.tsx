a542d3858c305459a91c07964cf69005c6a8723d
Christopher4113 <ch.lam1328@gmail.com>
2025-11-03 03:31:22 -0500

    new changes

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -7,7 +7,6 @@ import axios from 'axios';
 import {toast} from 'react-hot-toast';
 
 const LoginPage = () => {
-  
   const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
